3a5d04a114f8ee2160fc1a0490f680f89c7d245b
Bryce21903 <157423481+Bryce21903@users.noreply.github.com>
2024-05-01 12:17:01 -0400

    Update install_streamlit_packages.sh

--- install_streamlit_packages.sh
+++ install_streamlit_packages.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-# Install streamlit
-pip install streamlit
-# Install pandas
-pip install pandas
-# Install matplotlib
-pip install matplotlib
-# Install seaborn
-pip install seaborn
-# Install gdown
-pip install gdown
-# Install numpy
 pip install numpy
-# Install requests
+pip install gdown
+pip intall matplotlib
+pip install seaborn
+pip install pandas
 pip install requests
-# Install plotly
 pip install plotly
-bash install_streamlit_packages.sh
