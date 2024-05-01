e9d6176ade44b61931d99f70875832a7fb3ea350
Bryce21903 <157423481+Bryce21903@users.noreply.github.com>
2024-05-01 17:33:10 -0400

    Delete install_streamlit_packages.sh

--- install_streamlit_packages.sh
+++
@@ -1,8 +0,0 @@
-pip install numpy
-pip install gdown
-pip install matplotlib
-pip install seaborn
-pip install pandas
-pip install requests
-pip install plotly
-pip install --no-cache-dir --force-reinstall numpy
